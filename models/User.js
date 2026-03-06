046a1ff4f32dafe8026900acce5219910ec2125f
L3ryx <94659000+L3ryx@users.noreply.github.com>
2026-03-06 09:56:05 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,15 +1,26 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+const UserSchema = new mongoose.Schema({
+
+email: {
+type: String,
+required: true,
+unique: true
+},
+
+password: {
+type: String,
+required: true
+},
+
+stripeCustomerId: {
+type: String
+},
+
+defaultPaymentMethod: {
+type: String
+}
+
 }, { timestamps: true });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
