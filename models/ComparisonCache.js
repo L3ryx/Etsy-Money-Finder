55bbd8c37aad1ef5940d975ab94f5d89ff7b8c96
L3ryx <94659000+L3ryx@users.noreply.github.com>
2026-03-07 01:12:31 +0100

    Update ComparisonCache.js

--- models/ComparisonCache.js
+++ models/ComparisonCache.js
@@ -1,25 +1,19 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const ComparisonSchema = new mongoose.Schema({
-
-imageA:{
-type:String,
-index:true
-},
-
-imageB:{
-type:String,
-index:true
-},
-
-similarity:Number,
-
-createdAt:{
-type:Date,
-default:Date.now,
-expires:60 * 60 * 24 * 7 // 🔥 expire après 7 jours
-}
-
+const ComparisonCacheSchema = new mongoose.Schema({
+  imageA: { type: String, required: true },
+  imageB: { type: String, required: true },
+  similarity: { type: Number, required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 60 * 60 * 24 * 7 // auto delete after 7 days
+  }
 });
 
-module.exports = mongoose.model("ComparisonCache", ComparisonSchema);
+const ComparisonCache = mongoose.model(
+  "ComparisonCache",
+  ComparisonCacheSchema
+);
+
+export default ComparisonCache;
